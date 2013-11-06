66dde42aadd0d008c91fb1fb5a252ed57aafe458
Sarah Kim <jiyunie13@gmail.com>
2013-11-05 20:40:23 -0800

    Added Javascript, changed link colors
    I changed link colors from light blue and light green to dark green. I also added basic Javascript functionality to links by using jQuery.

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+/*	$('.header-link').hover(function() {
+		$(this).css({'color': '#A6D785'});
+	}, function() {
+		$(this).css({'color': 'white'});
+	});
+*/
+    $('.linkheader').hover(function() {
+        $(this).css({'color': '#7fa958'});
+    }, function() {
+        $(this).css({'color': '#629632'});
+    });
+
+    $('.link').hover(function() {
+        $(this).css({'color': '#7fa958'});
+    }, function() {
+        $(this).css({'color': '#629632'});
+    });
+
+    $('.credit').hover(function() {
+        $(this).css({'color': '#7fa958'});
+    }, function() {
+        $(this).css({'color': '#629632'});
+    });
+
+});
