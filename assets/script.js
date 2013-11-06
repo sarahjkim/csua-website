618dbdd35a96f1a452afda6d1b4bd3d9b2f82ef6
Sarah Kim <jiyunie13@gmail.com>
2013-11-06 10:32:33 -0800

    Created Events Panel
    I created the events panel (with switchable tabs). I just need to populate the different tabs now.

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,27 @@ $(document).ready(function(){
         $(this).css({'color': '#629632'});
     });
 
+    $('.event-name').hover(function() {
+        $(this).css({'color': 'grey'});
+    }, function() {
+        $(this).css({'color': 'black'});
+    });
+
     $('.credit').hover(function() {
         $(this).css({'color': '#7fa958'});
     }, function() {
         $(this).css({'color': '#629632'});
+    });
+
+    $('.tab').click(function() {
+        $('.tab').css({'color': 'grey'});
+        $(this).css({'color': 'white'});
+    });
+
+    $('#panels a').click(function() {
+        var panel_id = $(this).attr('href');
+        $('.panel').hide();
+        $(panel_id).show();
     });
 
 });
